c5c95b2e9854d2f04585b3a11087970c1791065a
Matthew Hertzfeld <>
2020-03-02 16:03:48 -0500

    Added new project for .Net Compact Framework 3.5.
    Cloned original GuerrillaNtp project and made nessisary changes.
    
    New project was built using VS2008, latest version of VS compatible is CF.

---
+++ GuerrillaNtp_CF/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GuerrillaNtp")]
+[assembly: AssemblyDescription("App-embedded NTP client for time-sensitive apps")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Robert Važan")]
+[assembly: AssemblyProduct("GuerrillaNtp")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("Copyright ©  2014-2020 Robert Važan")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e59126d9-8544-41e5-8205-e385dc561133")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.4.1.0")]
+
